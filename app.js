aa67d3832da364bcba613504717ffa739ce6a8db
yong <souyong2008@gmail.com>
2018-06-19 07:54:02 -0700

    added find and delete

---
+++ app.js
@@ -0,0 +1,36 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (erro, client) => {
+    if (erro) {
+        return console.log('unable to connect to mongodb');
+    }
+    console.log('connect to mongodb');
+
+
+    // to get db in mango3
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find().toArray().then((docs)=> {
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('there is an error, unable to fetch the data');
+    // });
+
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({text: 'test'}).then((result)=> {
+    //     console.log(result);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text: 'eat lunch'}).then(result => {
+    //     console.log(result);
+    // });
+
+    // findOneAndDelete
+    db.collection('Todos').findOneAndDelete({text: 'lol', completed: true}).then(result => {
+        console.log(result);
+    });
+
+    // db.close();
+});
